edfe0f3549471449e4fa48aebc35295c41e64675
Felipe Soares <soaresfelipef@gmail.com>
2016-03-27 15:15:36 -0300

    Project - V1

---
+++ GradientBackground/CAGradientLayer+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  CAGradientLayer+Convenience.swift
+//  GradientBackground
+//
+//  Created by Felipe Soares on 27/03/16.
+//  Copyright © 2016 Felipe Soares. All rights reserved.
+//
+
+import Foundation
